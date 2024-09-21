7c5a979c3453541e5ff434f77225bb7ab9e261af
Jon Durrant <jon@jondurrant.com>
2024-09-21 07:40:46 +0100

    Added a FreeRTOS Example

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # RPIPico2-Exp
 Experiments with Raspberry Pi Pico 2
+
+This is a set of initial experiments and tests with the Raspberry Pi Pico 2 and RP2350 published along with my VLOG on YouTube [DrJonEA](https://youtube.com/@drjonea)
+
+## Build process
+Though these should build with the VSCode extention, all my development is done using an external toolchain.
+
+I am using SDK 2.0.0 the location of which is published via the  environment variable: PICO_SDK_PATH
+
+I am using a shared copy of PicoTool using the environment variable: PICOTOOL_FETCH_FROM_GIT_PATH
+
+```
+mkdir build
+cd build
+cmake ..
+make
+```
+Binaries are in the folder: build/src/
++ *.elf: for SWD flashing
++ *.uf2 for bootself flashing
+
+ 
+## Example
+
+### Blink
+
+ Blink on board LED on a Pico 2 using the SDK
+ 
+### PI1Core
+ 
+ Performance test the Pico 2 for Integer arithmetic calculation the value of PI to 1000 decimal place. How many times can it complete this in 1 minute.
+ 
+ This uses the library [pi_spigot]( https://github.com/imahjoub/pi_spigot) to calculate PI
+
+### PI2Core
+ 
+ Performance test the Pico 2 for Integer arithmetic calculation the value of PI to 1000 decimal place. How many times can it complete this in 1 minute., this time running across both cores of the Pico.
+ 
+### FPUPerf
+
+Test floating point performance of the Pico2 and RP2350 by calculating a Forward Kinematic chain for a  robot arm, to give the end actuators pose. How many times can this be calculated in 10 seconds.
+
+This uses the [Eigen](https://gitlab.com/libeigen/eigen) library for matrix maths.
+
+### FPUPerf2040
+
+A RP2040 and Pico 1 version of FPUPerf for comparison perposes.
+
+### FreeRTOSBlink
+
+A FreeRTOS Blink example to demonstrate FreeRTOS on the RP2350 and Pico 2. Will Blink an LED on GPIO 2.
+
+This uses a shared copy of the Raspberry Pi fork for [FreeRTOS kernel](https://github.com/raspberrypi/FreeRTOS-Kernel) located through the environment variable: PICO_FREERTOS
+
+ 
+ 
+ 
+ 
---
+++ examples/FreeRTOSBlink/src/main.cpp
@@ -0,0 +1,126 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 21-Sep-2024
+ *      Author: jondurrant
+ */
+
+#include <cstdio>
+
+
+#include "BlinkAgent.h"
+
+//Standard Task priority
+#define TASK_PRIORITY		( tskIDLE_PRIORITY + 1UL )
+
+#define GP_BLINK 2
+
+
+/***
+ * Debug function to look at Task Stats
+ */
+void runTimeStats(   ){
+	TaskStatus_t *pxTaskStatusArray;
+	volatile UBaseType_t uxArraySize, x;
+	unsigned long ulTotalRunTime;
+
+
+   // Get number of takss
+   uxArraySize = uxTaskGetNumberOfTasks();
+   printf("Number of tasks %d\n", uxArraySize);
+
+   //Allocate a TaskStatus_t structure for each task.
+   pxTaskStatusArray = (TaskStatus_t *)pvPortMalloc( uxArraySize * sizeof( TaskStatus_t ) );
+
+   if( pxTaskStatusArray != NULL ){
+      // Generate raw status information about each task.
+      uxArraySize = uxTaskGetSystemState( pxTaskStatusArray,
+                                 uxArraySize,
+                                 &ulTotalRunTime );
+
+	 // Print stats
+	 for( x = 0; x < uxArraySize; x++ )
+	 {
+		 printf("Task: %d \t cPri:%d \t bPri:%d \t hw:%d \t%s\n",
+				pxTaskStatusArray[ x ].xTaskNumber ,
+				pxTaskStatusArray[ x ].uxCurrentPriority ,
+				pxTaskStatusArray[ x ].uxBasePriority ,
+				pxTaskStatusArray[ x ].usStackHighWaterMark ,
+				pxTaskStatusArray[ x ].pcTaskName
+				);
+	 }
+
+
+      // Free array
+      vPortFree( pxTaskStatusArray );
+   } else {
+	   printf("Failed to allocate space for stats\n");
+   }
+
+   //Get heap allocation information
+   HeapStats_t heapStats;
+   vPortGetHeapStats(&heapStats);
+   printf("HEAP avl: %d, blocks %d, alloc: %d, free: %d\n",
+		   heapStats.xAvailableHeapSpaceInBytes,
+		   heapStats.xNumberOfFreeBlocks,
+		   heapStats.xNumberOfSuccessfulAllocations,
+		   heapStats.xNumberOfSuccessfulFrees
+		   );
+}
+
+
+/***
+ * Main task to boot the other Agents
+ * @param params - unused
+ */
+void mainTask(void *params){
+	BlinkAgent blink(GP_BLINK);
+
+	printf("Boot task started\n");
+	blink.start("Blink", TASK_PRIORITY);
+
+	for (;;){
+		vTaskDelay(2000);
+		 runTimeStats();
+	}
+}
+
+
+/***
+ * Launch the tasks and scheduler
+ */
+void vLaunch( void) {
+
+	//Start blink task
+    TaskHandle_t task;
+    xTaskCreate(mainTask, "MainThread", 500, NULL, TASK_PRIORITY, &task);
+
+    /* Start the tasks and timer running. */
+    vTaskStartScheduler();
+}
+
+
+
+
+int main(){
+
+
+	stdio_init_all();
+	sleep_ms(1000);
+
+	printf("Go\n");
+
+    //Start tasks and scheduler
+    const char *rtos_name = "FreeRTOS";
+    printf("Starting %s on core 0:\n", rtos_name);
+    vLaunch();
+
+	for (;;){
+		sleep_ms(3000);
+	}
+
+
+
+}
+
+
